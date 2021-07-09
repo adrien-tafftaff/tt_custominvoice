b017725222a2c7ccf15fc51764aa9fc867f10dda
adrien <adrien@tafftaff.fr>
2021-07-09 21:58:22 +0200

    add fr translation
    add french translation

---
+++ translations/fr.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_2e03bca91fe62c760887f77dbec51270'] = 'Personnalisation facture';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_411920ffb53de4b531b353fdc1e68234'] = 'ajouter du contenu sur vos contenu en fonction du pays';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_93cba07454f06a4a960172bbd6e2a435'] = 'oui';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_bafd7322c6e97d25b6299b5d6fe8920b'] = 'non';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_f4f70727dc34561dfde1a3c529b6205c'] = 'paramètres';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_3acb228e77768f2611ae59e04595a90d'] = 'Titre du tableau';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_c390087dcb976888932fcd9850c7df92'] = 'Contenu du tableau ou note';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_a8db67f9181b1a449d5085d6e550210e'] = 'Texte Libre';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_8d9bf420b7b9e6e3c5ff99278a178919'] = 'Pays sur lequel afficher';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_4d93089f633a2afb4311205b1c25f898'] = 'Le titre ne sera pas utilisé';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_48d7fcad15ea82967eac2680760f6a8a'] = 'Ajouter uniquemebnt le texte en note';
+$_MODULE['<{tt_custominvoice}prestashop>tt_custominvoice_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{tt_custominvoice}prestashop>configure_2e03bca91fe62c760887f77dbec51270'] = 'Personnalisation de facture';
+$_MODULE['<{tt_custominvoice}prestashop>configure_50d1aaaac55b3ad43d2774bbfd3e19bc'] = 'Ajouter du texte à vos factures';
+$_MODULE['<{tt_custominvoice}prestashop>configure_f24e80e9f13511ca3867bf4027d7d2e3'] = 'Ce module vous laisse ajouter du texte sur votre facture pour un pay actif';
+$_MODULE['<{tt_custominvoice}prestashop>configure_1315d6c7c9585a8672869a1650586e2c'] = 'Faîtes attention en utilisant ce module';
+$_MODULE['<{tt_custominvoice}prestashop>configure_3a00dad3961d0f21c8ff15dff76bf109'] = 'Les notes ne peuvent pas être supprimés pour respecter les lois en vigueur dans certains pays';
